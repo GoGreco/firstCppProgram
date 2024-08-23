8345eeb17489d8430936c35474430d2d133201e0
Pedro Lucas Rodrigues Greco <rgreco.pedro@dinfor.uniceub>
2024-08-23 07:14:47 -0300

    Written my first, simple and ugly but mine nonetheless, program in c++, and explained it

---
+++ main.cpp
@@ -0,0 +1,20 @@
+//include the standard library //
+//Input Output Stream// 
+#include <iostream>
+
+
+//the main function, were the execution happens
+//any funtion writen must jave specified what type of data it will return
+// before the funtion name that is
+//inside the () we put parameters of the function 
+
+int main() {
+    //calls the library (std for standard)
+    //defines the command
+    //sta(Standard) :: (wich command will be used) cout(character out)
+    // this is called a statement, it finishes with ';' (fuck python)
+    std::cout << "Hello World";
+
+    //returns 0 because the OS will only know the program was successfully executed if it returns what it was defined to output
+    return 0
+}
